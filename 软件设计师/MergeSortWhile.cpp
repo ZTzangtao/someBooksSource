7605229854b9068061128b7548265afd1822e417
Tommy <zangtao3211@163.com>
2024-04-18 22:25:10 +0800

    1

--- 软件设计师/MergeSortWhile.cpp
+++ 软件设计师/MergeSortWhile.cpp
@@ -1,2 +1,17 @@
 #include <stdio.h>
 #include <math.h>
+
+
+void mergeSort(int a[], int p, int r) {
+    int q;
+    if (p < r) {
+        q = (p + r) / 2;
+        mergeSort(a, p, q);
+        mergeSort(a, q + 1, r);
+        merge(a,p, q, r);
+    }
+}
+
+void merge(int a[], int p, int q, int r) {
+
+}

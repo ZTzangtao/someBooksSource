9d96b4c81ce54ed5a897fe7a58c7aa7710b22804
Tommy <zangtao3211@163.com>
2024-04-19 23:57:56 +0800

    1

--- 软件设计师/MergeSortWhile.cpp
+++ 软件设计师/MergeSortWhile.cpp
@@ -12,6 +12,26 @@ void mergeSort(int a[], int p, int r) {
     }
 }
 
-void merge(int a[], int p, int q, int r) {
-
+void merge(int A[], int p, int q, int r) {
+    int n1 = q - p + 1, n2 = r - q, i, j, k;
+    int L[50], R[50];
+    for (i = 0; i < n1; i++) {
+        L[i] = A[p + i];
+    }
+    for (j = 0; j < n2; j++) {
+        R[j] = A[q + j +1];
+    }
+    L[n1] = INT_MAX;
+    R[n2] = INT_MAX;
+    i = 0;
+    j = 0;
+    for (k = p; k < r + 1; k++) {
+        if (L[i] < R[j]) {
+            A[k] = L[i];
+            i++;
+        } else {
+            A[k] = R[j];
+            j++;
+        }
+    }
 }
